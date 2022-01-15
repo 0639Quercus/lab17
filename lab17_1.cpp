386d59f125551c48bbb511eeb00a1a3d392feba9
0639Quercus <thsomsits.brie@gmail.com>
2022-01-15 12:21:58 +0700

    lab17_1.cpp completed

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,40 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *dPtr, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(dPtr+i);
+        if((i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *dPtr, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        *(dPtr+i) = rand()%101 / 100.;
+    }
+}
+
+void findRowSum(const double *dPtr, double *rowSum, int N, int M){
+	double sum;
+	for(int i = 0; i < N; i++){
+		sum = 0;
+		for(int j = 0; j < M; j++){
+			sum += *(dPtr + (i*M) + j);
+		}
+		*(rowSum+i) = sum;
+	}
+}
+
+void findColSum(const double *dPtr, double *colSum, int N, int M){
+	double sum;
+	for(int i = 0; i < M; i++){
+		sum = 0;
+		for(int j = 0; j < N; j++){
+			sum += *(dPtr + (j*M) + i);
+		}
+		*(colSum+i) = sum;
+	}
+}
